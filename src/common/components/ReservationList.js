5ea055d8ad70b7f8a5d86429b048ec0e96e5a91f
Adam Nathaniel Davis <adamdavis@filevine.com>
2022-02-07 11:37:51 -0500

    design complete

--- src/common/components/ReservationList.js
+++ src/common/components/ReservationList.js
@@ -5,23 +5,71 @@ import { Column, Row } from "@toolz/material-ui/dist/index";
 import { css3 } from "@toolz/css3/src/css3";
 import { use } from "../objects/use";
 
-// TODO: style to match the mock-up
 export const ReservationList = ({ reservations }) => {
   const viewport = useViewport(materialUiBreakpoints);
   const endpointReservations = use.reservationsEndpoint;
 
   const getResponsiveDisplay = () => {
     return reservations.map((reservation) => {
-      console.log(reservation);
       const thisReservation = endpointReservations.reservations.find(
         (endpointReservation) => endpointReservation.id === reservation.id
       );
-      console.log(thisReservation);
+      const startTime = new Date(thisReservation.start)
+        .toLocaleTimeString()
+        .replaceAll(":00", "")
+        .replaceAll(" ", "");
+      const endTime = new Date(thisReservation.end)
+        .toLocaleTimeString()
+        .replaceAll(":00", "")
+        .replaceAll(" ", "");
+      const date = new Date(thisReservation.start).toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      });
       if (viewport.size === "xs" || viewport.size === "sm") {
         return (
-          <Column xs={12} sm={12}>
-            Mobile
-          </Column>
+          <div
+            key={thisReservation.id}
+            style={{
+              border: "1px solid #eeeeee",
+              boxShadow: "2px 2px 2px #eeeeee",
+              marginBottom: 16,
+              padding: 16,
+            }}
+          >
+            <Row>
+              <Column xs={12} sm={12}>
+                <div
+                  style={{
+                    maxHeight: 200,
+                    maxWidth: "100%",
+                    overflow: css3.overflow.hidden,
+                  }}
+                >
+                  <img
+                    alt={"room"}
+                    src={thisReservation.room.imageUrl}
+                    style={{
+                      width: "100%",
+                      position: css3.position.relative,
+                      bottom: 200,
+                    }}
+                  />
+                </div>
+                <div
+                  style={{
+                    fontSize: css3.fontSize.large,
+                    fontWeight: css3.fontWeight._700,
+                  }}
+                >
+                  {startTime} - {endTime}
+                </div>
+                <div>{date}</div>
+                <div>{thisReservation.room.name}</div>
+              </Column>
+            </Row>
+          </div>
         );
       } else {
         return (
@@ -35,8 +83,23 @@ export const ReservationList = ({ reservations }) => {
             }}
           >
             <Row>
-              <Column md={10} lg={10} xl={10}>
-                Left
+              <Column md={2} lg={2} xl={2}>
+                <img
+                  alt={"room"}
+                  src={thisReservation.room.imageUrl}
+                  style={{ width: "100%" }}
+                />
+              </Column>
+              <Column md={8} lg={8} xl={8} style={{ paddingLeft: 16 }}>
+                <div
+                  style={{
+                    fontSize: css3.fontSize.large,
+                    fontWeight: css3.fontWeight._700,
+                  }}
+                >
+                  {startTime} - {endTime}
+                </div>
+                <div>{date}</div>
               </Column>
               <Column md={2} lg={2} xl={2}>
                 <div style={{ float: css3.float.right }}>
@@ -50,7 +113,5 @@ export const ReservationList = ({ reservations }) => {
     });
   };
 
-  console.log(viewport);
-  console.log(reservations);
   return getResponsiveDisplay();
 };
