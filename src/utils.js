9d7e3c36ca67cb475f6ed0a19085a75080aa50a7
Adam Nathaniel Davis <adamdavis@filevine.com>
2022-02-07 12:58:14 -0500

    finished util and tests

--- src/utils.js
+++ src/utils.js
@@ -5,9 +5,34 @@
  *   - reservations conflict if their times overlap in any way
  *   - reservations DO NOT conflict if they are just touching each other (reservation1.end === reservation2.start)
  */
-// eslint-disable-next-line no-unused-vars
 export const isScheduleConflict = (reservations) => {
-  // TODO: complete the implementation of this func
-
-  return false;
+  let conflictExists = false;
+  reservations.forEach((firstReservation, firstIndex) => {
+    reservations.forEach((secondReservation, secondIndex) => {
+      if (
+        conflictExists ||
+        firstReservation?.room.name !== secondReservation?.room.name ||
+        secondIndex <= firstIndex
+      )
+        return;
+      const firstReservationStartTime = new Date(
+        firstReservation.start
+      ).getTime();
+      const firstReservationEndTime = new Date(firstReservation.end).getTime();
+      const secondReservationStartTime = new Date(
+        secondReservation.start
+      ).getTime();
+      const secondReservationEndTime = new Date(
+        secondReservation.end
+      ).getTime();
+      if (
+        (secondReservationStartTime > firstReservationStartTime &&
+          secondReservationStartTime < firstReservationEndTime) ||
+        (secondReservationEndTime > firstReservationStartTime &&
+          secondReservationEndTime < firstReservationEndTime)
+      )
+        conflictExists = true;
+    });
+  });
+  return conflictExists;
 };
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,9 +1,50 @@
 import { isScheduleConflict } from "./utils";
+import { sampleReservations } from "./common/arrays/sample.reservations";
 
 describe("isScheduleConflict", () => {
   it("returns [false] for an empty list", () => {
     expect(isScheduleConflict([])).toBe(false);
   });
 
-  // TODO: add more test coverage
+  it("returns [false] for sampleReservations", () => {
+    expect(isScheduleConflict(sampleReservations)).toBe(false);
+  });
+
+  const reservationsWithConflictingEndTime = [
+    ...sampleReservations,
+    {
+      id: "d4297a67-cfde-4841-81a8-aa90f719255c",
+      start: "2021-02-03T14:30:00.000Z",
+      end: "2021-02-03T15:35:00.000Z",
+      room: {
+        id: "401662b9-b110-4159-a4f2-ebd955f3b1f4",
+        name: "Room A",
+        imageUrl:
+          "https://staging.cove.is/parse/files/hRKEvW2lN74k5nCg6p2XtmiWRNHycE2pHpXpELMX/f96efd3f11aadb34135bb1f0aecf9667_Quincy%20Room.jpg",
+      },
+    },
+  ];
+
+  it("returns [true] for reservationsWithConflictingEndTime", () => {
+    expect(isScheduleConflict(reservationsWithConflictingEndTime)).toBe(true);
+  });
+
+  const reservationsWithConflictingStartTime = [
+    ...sampleReservations,
+    {
+      id: "d4297a67-cfde-4841-81a8-aa90f719255d",
+      start: "2021-02-03T14:35:00.000Z",
+      end: "2021-02-03T15:45:00.000Z",
+      room: {
+        id: "401662b9-b110-4159-a4f2-ebd955f3b1f4",
+        name: "Room A",
+        imageUrl:
+          "https://staging.cove.is/parse/files/hRKEvW2lN74k5nCg6p2XtmiWRNHycE2pHpXpELMX/f96efd3f11aadb34135bb1f0aecf9667_Quincy%20Room.jpg",
+      },
+    },
+  ];
+
+  it("returns [true] for reservationsWithConflictingStartTime", () => {
+    expect(isScheduleConflict(reservationsWithConflictingStartTime)).toBe(true);
+  });
 });
